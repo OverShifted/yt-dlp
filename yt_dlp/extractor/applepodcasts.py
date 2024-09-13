3b164882012e0c391a03563571191d79b341bf9d
OverShifted <prowidgs@gmail.com>
2024-09-13 14:27:11 +0330

    [ie/ApplePodcasts] Fix extractor

--- yt_dlp/extractor/applepodcasts.py
+++ yt_dlp/extractor/applepodcasts.py
@@ -1,11 +1,11 @@
 from .common import InfoExtractor
 from ..utils import (
-    clean_html,
     clean_podcast_url,
-    get_element_by_class,
+    get_element_by_id,
     int_or_none,
     parse_iso8601,
-    try_get,
+    traverse_obj,
+    try_call,
 )
 
 
@@ -13,15 +13,15 @@ class ApplePodcastsIE(InfoExtractor):
     _VALID_URL = r'https?://podcasts\.apple\.com/(?:[^/]+/)?podcast(?:/[^/]+){1,2}.*?\bi=(?P<id>\d+)'
     _TESTS = [{
         'url': 'https://podcasts.apple.com/us/podcast/207-whitney-webb-returns/id1135137367?i=1000482637777',
-        'md5': '41dc31cd650143e530d9423b6b5a344f',
+        'md5': 'baf8a6b8b8aa6062dbb4639ed73d0052',
         'info_dict': {
             'id': '1000482637777',
             'ext': 'mp3',
             'title': '207 - Whitney Webb Returns',
-            'description': 'md5:75ef4316031df7b41ced4e7b987f79c6',
+            'description': 'md5:6388e95fe306e88fa61d85d683102944',
             'upload_date': '20200705',
             'timestamp': 1593932400,
-            'duration': 6454,
+            'duration': 5369,
             'series': 'The Tim Dillon Show',
             'thumbnail': 're:.+[.](png|jpe?g|webp)',
         },
@@ -39,47 +39,44 @@ class ApplePodcastsIE(InfoExtractor):
     def _real_extract(self, url):
         episode_id = self._match_id(url)
         webpage = self._download_webpage(url, episode_id)
-        episode_data = {}
-        ember_data = {}
-        # new page type 2021-11
-        amp_data = self._parse_json(self._search_regex(
-            r'(?s)id="shoebox-media-api-cache-amp-podcasts"[^>]*>\s*({.+?})\s*<',
-            webpage, 'AMP data', default='{}'), episode_id, fatal=False) or {}
-        amp_data = try_get(amp_data,
-                           lambda a: self._parse_json(
-                               next(a[x] for x in iter(a) if episode_id in x),
-                               episode_id),
-                           dict) or {}
-        amp_data = amp_data.get('d') or []
-        episode_data = try_get(
-            amp_data,
-            lambda a: next(x for x in a
-                           if x['type'] == 'podcast-episodes' and x['id'] == episode_id),
-            dict)
-        if not episode_data:
-            # try pre 2021-11 page type: TODO: consider deleting if no longer used
-            ember_data = self._parse_json(self._search_regex(
-                r'(?s)id="shoebox-ember-data-store"[^>]*>\s*({.+?})\s*<',
-                webpage, 'ember data'), episode_id) or {}
-            ember_data = ember_data.get(episode_id) or ember_data
-            episode_data = try_get(ember_data, lambda x: x['data'], dict)
-        episode = episode_data['attributes']
-        description = episode.get('description') or {}
 
-        series = None
-        for inc in (amp_data or ember_data.get('included') or []):
-            if inc.get('type') == 'media/podcast':
-                series = try_get(inc, lambda x: x['attributes']['name'])
-        series = series or clean_html(get_element_by_class('podcast-header__identity', webpage))
+        serialized_server_data = get_element_by_id('serialized-server-data', webpage)
+        serialized_server_data = self._parse_json(serialized_server_data, episode_id)
+        data = serialized_server_data[0]['data']
+
+        # Shelves are like "blocks" of data which appear to be ordered and
+        # organized similar to the visual ordering of data in the webpage
+        # when opened in a web browser.
+        shelves = data['shelves']
+        main_shelf = shelves[0]['items'][0]  # Contains thumbnail, audio stream url, ...
+        desc_shelf = traverse_obj(shelves, (1, 'items'))  # Contains the description
+        info_shelf = traverse_obj(shelves, (-1, 'items'))  # Contains additional info such as "show name" (aka "series")
+
+        episode = main_shelf['primaryButtonAction']['episodeOffer']
+
+        artwork = traverse_obj(main_shelf, ('primaryArtwork', 'artwork'))
+        artwork_template = artwork.get('template')
+        artwork_width = artwork.get('width')
+        artwork_height = artwork.get('height')
+
+        if isinstance(artwork_template, str) and artwork_width is not None and artwork_height is not None:
+            # We could also use python's built-in formatting, but that's too much flexible.
+            artwork_url = try_call(
+                lambda: artwork_template
+                .replace('{w}', str(artwork_width))
+                .replace('{h}', str(artwork_height))
+                .replace('{f}', 'webp'))
+        else:
+            artwork_url = None
 
         return {
             'id': episode_id,
-            'title': episode.get('name'),
-            'url': clean_podcast_url(episode['assetUrl']),
-            'description': description.get('standard') or description.get('short'),
-            'timestamp': parse_iso8601(episode.get('releaseDateTime')),
-            'duration': int_or_none(episode.get('durationInMilliseconds'), 1000),
-            'series': series,
-            'thumbnail': self._og_search_thumbnail(webpage),
+            'title': data.get('title') or episode['title'],
+            'url': clean_podcast_url(episode['streamUrl']),
+            'description': traverse_obj(desc_shelf, (0, 'text')) or self._og_search_description(webpage),
+            'timestamp': parse_iso8601(episode.get('releaseDate')),
+            'duration': int_or_none(episode.get('duration')),
+            'series': traverse_obj(info_shelf, (0, 'clickAction', 'title')),
+            'thumbnail': artwork_url or self._og_search_thumbnail(webpage),
             'vcodec': 'none',
         }
